2bc6b8179e4182efc39e67343f209ef22e042592
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-11-18 14:57:29 +0100

    Sync progress

--- leviathan-common/src/util.rs
+++ leviathan-common/src/util.rs
@@ -1,4 +1,7 @@
-use std::fmt::{Debug, Display};
+use std::{
+    collections::{HashMap, LinkedList},
+    fmt::{Debug, Display},
+};
 
 #[derive(Clone, Copy)]
 pub struct TextPosition {
@@ -21,5 +24,112 @@ impl Display for TextPosition {
 impl Debug for TextPosition {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(format!("(line {}, column {})", self.line, self.column).as_str())
+    }
+}
+
+pub struct NamespacedTree<V: Sized> {
+    root: HashMap<String, NamespacedTreeNode<V>>,
+}
+
+impl<V> NamespacedTree<V> {
+    pub fn new() -> Self {
+        Self {
+            root: HashMap::new(),
+        }
+    }
+
+    pub fn get(&self, key: &String) -> Option<&NamespacedTreeNode<V>> {
+        self.root.get(key)
+    }
+
+    pub fn insert(
+        &mut self,
+        key: String,
+        value: NamespacedTreeNode<V>,
+    ) -> Option<NamespacedTreeNode<V>> {
+        self.root.insert(key, value)
+    }
+
+    pub fn get_mut(&mut self, key: &String) -> Option<&mut NamespacedTreeNode<V>> {
+        self.root.get_mut(key)
+    }
+
+    pub fn collect_vec(self) -> Vec<V> {
+        let mut vec = Vec::with_capacity(0);
+        for root_element in self.root {
+            root_element.1.collect_vec(&mut vec);
+        }
+        vec
+    }
+}
+
+pub enum NamespacedTreeNode<V: Sized> {
+    Leaf {
+        elements: LinkedList<V>,
+    },
+    TinyLeaf(V),
+    Branch {
+        elements: LinkedList<V>,
+        subnodes: HashMap<String, NamespacedTreeNode<V>>,
+    },
+    BareBranch {
+        subnodes: HashMap<String, NamespacedTreeNode<V>>,
+    },
+    BranchExtension {
+        package: String,
+        link: Box<NamespacedTreeNode<V>>,
+    },
+}
+
+impl<V> NamespacedTreeNode<V> {
+    pub fn empty_leaf() -> Self {
+        Self::Leaf {
+            elements: LinkedList::new(),
+        }
+    }
+
+    pub fn tiny_leaf(value: V) -> Self {
+        Self::TinyLeaf(value)
+    }
+
+    pub fn empty_branch() -> Self {
+        Self::Branch {
+            elements: LinkedList::new(),
+            subnodes: HashMap::with_capacity(0),
+        }
+    }
+
+    pub fn empty_bare_branch() -> Self {
+        Self::BareBranch {
+            subnodes: HashMap::with_capacity(0),
+        }
+    }
+
+    pub fn branch_extension(package: String, link: NamespacedTreeNode<V>) -> Self {
+        Self::BranchExtension {
+            package,
+            link: Box::new(link),
+        }
+    }
+
+    pub fn collect_vec(self, vec: &mut Vec<V>) {
+        match self {
+            NamespacedTreeNode::Leaf { elements } => {
+                elements.into_iter().for_each(|it| vec.push(it));
+            }
+            NamespacedTreeNode::TinyLeaf(element) => {
+                vec.push(element);
+            }
+            NamespacedTreeNode::Branch { elements, subnodes } => {
+                elements.into_iter().for_each(|it| vec.push(it));
+                subnodes.into_iter().for_each(|it| it.1.collect_vec(vec));
+            }
+            NamespacedTreeNode::BareBranch { subnodes } => {
+                subnodes.into_iter().for_each(|it| it.1.collect_vec(vec));
+            }
+            NamespacedTreeNode::BranchExtension { package: _, link } => {
+                link.collect_vec(vec);
+            }
+        }
     }
 }
--- leviathan-compiler/src/resolver.rs
+++ leviathan-compiler/src/resolver.rs
@@ -1,29 +1,40 @@
-use leviathan_common::structure::{Function, Structure};
-use std::collections::{HashMap, HashSet, LinkedList};
+use leviathan_common::{
+    prelude::*,
+    structure::{Function, Structure},
+    util::NamespacedTree,
+};
+use std::collections::LinkedList;
 
-pub struct Resolver<'a> {
-    _function_pool: HashSet<Function>,
-    _functions: HashMap<String, FunctionOverloader<'a>>,
-    _types: HashMap<String, () /* TODO */>,
+pub struct Resolver {
+    tree: NamespacedTree<FunctionOverloader>,
 }
 
-impl Resolver<'_> {
+impl Resolver {
     pub fn new() -> Self {
         Self {
-            _function_pool: HashSet::new(),
-            _functions: HashMap::new(),
-            _types: HashMap::new(),
+            tree: NamespacedTree::new(),
         }
     }
 
-    pub fn add(&mut self, _structure: Structure) {
-        todo!()
+    pub fn load_structure(&mut self, structure: Structure) -> Result<()> {
+        let Structure {
+            namespace,
+            imports,
+            functions,
+        } = structure;
+        if namespace.packages.len() < 1 {
+            return Err(Error::Generic(
+                "Unable to resolve structure with invalid namespace".into(),
+            ));
+        }
+        self.tree.get_mut(&namespace.packages[0]);
+        Ok(())
     }
 }
 
-pub struct FunctionOverloader<'a>(LinkedList<&'a Function>);
+pub struct FunctionOverloader(LinkedList<Function>);
 
-impl<'a> FunctionOverloader<'a> {
+impl FunctionOverloader {
     pub fn new() -> Self {
         Self(LinkedList::new())
     }
