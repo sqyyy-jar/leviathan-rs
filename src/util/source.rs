b589020a4b6d5144339d58f3bc1237ee35a684c6
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-03-11 18:22:59 +0100

    Add utils

---
+++ src/util/source.rs
@@ -0,0 +1,34 @@
+use std::{iter::Peekable, ops::Range, str::Chars};
+
+pub struct Source<'a> {
+    source: &'a str,
+    chars: Peekable<Chars<'a>>,
+    pub index: usize,
+}
+
+impl<'a> Source<'a> {
+    pub fn new(source: &'a str) -> Self {
+        Self {
+            source,
+            chars: source.chars().peekable(),
+            index: 0,
+        }
+    }
+
+    pub fn has_next(&mut self) -> bool {
+        self.chars.peek().is_some()
+    }
+
+    pub fn peek(&mut self) -> char {
+        *self.chars.peek().unwrap()
+    }
+
+    pub fn eat(&mut self) {
+        let c = self.chars.next().unwrap();
+        self.index += c.len_utf8();
+    }
+
+    pub fn str(&mut self, span: Range<usize>) -> &'a str {
+        &self.source[span]
+    }
+}
