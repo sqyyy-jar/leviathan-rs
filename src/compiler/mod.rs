7551c230c420a5897b1cb7da1efee0e0c06247db
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-03-12 12:45:47 +0100

    Add compilation setup

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -1,4 +1,12 @@
+use crate::parser::Node;
+
+use self::{
+    collecting::CollectedModule,
+    error::{Error, Result},
+};
+
 pub mod collecting;
+pub mod error;
 
 pub struct CompileTask {
     pub state: State,
@@ -7,14 +15,30 @@ pub struct CompileTask {
 impl Default for CompileTask {
     fn default() -> Self {
         Self {
-            state: State::LayoutCollecting {},
+            state: State::LayoutCollecting {
+                modules: Vec::with_capacity(0),
+            },
         }
     }
 }
 
+impl CompileTask {
+    pub fn include(&mut self, BareModule { name, root }: BareModule) -> Result<()> {
+        let State::LayoutCollecting { modules } = &mut self.state else {
+            return Err(Error::InvalidOperation);
+        };
+        Ok(())
+    }
+}
+
 pub enum State {
-    LayoutCollecting {},
+    LayoutCollecting { modules: Vec<CollectedModule> },
     IntermediaryGeneration {},
     DependencyFiltering {},
     BinaryAssembling {},
+}
+
+pub struct BareModule {
+    pub name: String,
+    pub root: Node,
 }
