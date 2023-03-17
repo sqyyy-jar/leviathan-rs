bbae932970c20d35234fc4ca5e02b51b86164011
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-03-17 18:23:53 +0100

    Add testing setup

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,7 @@ pub mod parser;
 pub mod util;
 
 fn main() {
-    let src = r#"
-(mod assembly)
-(scope abc 1
-"#;
+    let src = include_str!("../main.lvt");
     let src2 = r#"
 (mod assembly)
 (static buf
@@ -22,8 +19,11 @@ fn main() {
     let ast = build_ast("testing".into(), tokens).unwrap_or_else(|err| err.abort("main.lvt"));
     let ast2 = build_ast("testing2".into(), tokens2).unwrap_or_else(|err| err.abort("main.lvt"));
     let mut task = CompileTask::default();
-    task.include(ast).unwrap();
-    task.include(ast2).unwrap();
-    task.gen_intermediary().unwrap();
+    task.include(ast)
+        .unwrap_or_else(|err| err.abort("main.lvt"));
+    task.include(ast2)
+        .unwrap_or_else(|err| err.abort("main.lvt"));
+    task.gen_intermediary()
+        .unwrap_or_else(|err| err.abort("main.lvt"));
     dbg!(task);
 }
